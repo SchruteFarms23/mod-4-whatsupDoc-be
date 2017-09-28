58cf9d0de2c4da41eb34b2d4c3d24f04fc64da0e
SchruteFarms23 <romanmullady@gmail.com>
2017-09-28 17:38:59 -0400

    backend signup and login

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,8 @@
+class AuthController < ApplicationController
+  def create
+    user= User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      render json: user
+    end
+  end
+end
